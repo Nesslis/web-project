b92fc0d84e0961b8ffb42211063e1ffc5fe4c138
Nesslis <112620849+Nesslis@users.noreply.github.com>
2023-12-20 12:03:15 +0300

    Update: check.component.ts

--- src/app/check/check.component.ts
+++ src/app/check/check.component.ts
@@ -25,25 +25,21 @@ export class CheckComponent implements OnInit {
     if (this.checkForm.valid) {
       const data = this.checkForm.value;
       
-      // Make API call to check existing customer based on TC No or Email
       this.http.post<any>('http://213.248.166.144:7070/customer/getCustomerByTcNoEmail', data)
         .subscribe(
           (response) => {
             console.log('Customer check response:', response);
-            // Check if an existing customer is found
             if (response && response.length > 0) {
-              this.existingCustomer = response[0]; // Assuming response contains customer data
+              this.existingCustomer = response[0]; 
             } else {
-              this.existingCustomer = null; // Reset existingCustomer if no customer found
+              this.existingCustomer = null; 
             }
           },
           (error) => {
             console.error('Error while checking customer:', error);
-            // Handle the error response (e.g., 409 conflict, server error, etc.)
           }
         );
     } else {
-      // Form is invalid, handle accordingly (show validation messages, etc.)
       console.log('Form is invalid');
     }
   }
